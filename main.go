57ed817e37ad02ccac628e24b50cdcdcc423f626
whoisnian <zhuchangbao1998@gmail.com>
2021-11-29 01:29:36 +0800

    WIP: test/normal mode

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"io"
 	"os"
 
@@ -10,10 +12,43 @@ import (
 	"golang.org/x/term"
 )
 
-const device = "/dev/ttyUSB0"
+var isDebug = flag.Bool("d", false, "Output debug message")
+var isTest = flag.Bool("t", false, "Keycodes test mode")
+var device = flag.String("dev", "/dev/ttyUSB0", "UART device name")
 
-func main() {
-	port, err := serial.Open(device, 9600, 8, serial.ParityNone, serial.StopBits1)
+func initTerminal() (fd int, oldState *term.State) {
+	fd = int(os.Stdin.Fd())
+	oldState, err := term.MakeRaw(fd)
+	if err != nil {
+		panic(err)
+	}
+	return fd, oldState
+}
+
+func runTestMode(fd int) {
+	var buf [8]byte
+	comboMode := false
+	for {
+		n, err := unix.Read(fd, buf[:])
+		fmt.Printf("%x\r\n", buf[:n])
+		if comboMode {
+			comboMode = false
+			code := driver.CalcCombo(buf[:n])
+		} else if buf[0] == 0x0b {
+			comboMode = true
+		} else {
+
+		}
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func runNormalMode(fd int) {
+	port, err := serial.Open(*device, 9600, 8, serial.ParityNone, serial.StopBits1)
 	if err != nil {
 		panic(err)
 	}
@@ -32,25 +67,22 @@ func main() {
 	// 		} else if err != nil {
 	// 			panic(err)
 	// 		}
-	// 		fmt.Println(string(buf[:n]))
+	// 		fmt.Printf("%x\r\n", buf[:n])
 	// 	}
 	// }()
 
-	fd := int(os.Stdin.Fd())
-	oldState, err := term.MakeRaw(fd)
-	if err != nil {
-		panic(err)
-	}
-	defer term.Restore(fd, oldState)
-
 	var buf [8]byte
+	comboMode := false
 	for {
 		n, err := unix.Read(fd, buf[:])
-		if buf[0] == 1 { // input Ctrl-A to exit
-			break
+		// fmt.Printf("%x\r\n", buf[:n])
+		if buf[0] == 0x0b {
+			comboMode = true
+			continue
 		}
-		if res := driver.EncodeForCH9329_M(buf[:n]); len(res) > 0 {
+		if res := driver.EncodeForCH9329_K(buf[:n]); len(res) > 0 {
 			port.Write(res)
+			port.Write(driver.EncodeForCH9329_K(nil))
 		}
 		if err == io.EOF {
 			break
@@ -60,4 +92,15 @@ func main() {
 	}
 
 	// wg.Wait()
+}
+
+func main() {
+	fd, oldState := initTerminal()
+	defer term.Restore(fd, oldState)
+
+	if *isTest {
+		runTestMode(fd)
+	} else {
+		runNormalMode(fd)
+	}
 }
